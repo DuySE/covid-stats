1346dfd41b44b73479bc9a259291ca4e7796ad07
Long Hoàng <long.hoang@tiki.vn>
2020-09-30 21:38:55 +0700

    Fixing pagination bug

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Pagination from './components/Pagination';
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [summaries, setSummaries] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [currentSum, setcurrentSum] = useState([]);
+
   useEffect(() => {
     const fetchCountries = () => {
       fetch("https://api.covid19api.com/countries")
@@ -22,23 +25,26 @@ const App = () => {
         .then(response => response.json())
         .then(result => {
           setSummaries(result.Countries);
+          setTotal(result.Countries.length)
+          setcurrentSum(result.Countries.slice(0, 10))
         })
         .catch(error => console.log('error', error));
     }
     fetchCountries();
     fetchSummary();
   }, []);
-  const paginate = (summaries, currentIndex, summariesPerPage) => {
-    let start = (currentIndex * summariesPerPage) - summariesPerPage;
+  const paginate = (sum, currentIndex, summariesPerPage) => {
+    let start = (currentIndex * summariesPerPage);
     let end = start + summariesPerPage;
-    let currentList = summaries.slice(start, end);
-    setSummaries(currentList);
+    let currentList = sum.slice(start, end);
+    setcurrentSum(currentList);
   }
+
   return (
     <div className="App">
       <Autocomplete data={countries} />
-      <Table data={summaries} />
-      <Pagination data={summaries} onClick={paginate} />
+      <Table data={currentSum} />
+      <Pagination data={summaries} onClick={paginate} total={total} />
     </div>
   );
 }
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-const Pagination = ({ data, onClick }) => {
+const Pagination = ({ data, onClick, total }) => {
   const [pages, setPages] = useState([]);
-  let numberOfPages = Math.ceil(data.length / 10);
+  let numberOfPages = Math.ceil(total / 10);
   useEffect(() => {
     let pages = [];
     for (let i = 0; i < numberOfPages; i++) {
-      pages.push(<li key={i}><a href="/" onClick={onClick(data, i, 10)}> {i}</a></li>)
+      pages.push(<li key={i}><a href="#" onClick={() => onClick(data, i, 10)}> {i}</a></li>)
     }
     setPages(pages);
-  }, [data]);
+  }, [total]);
   return (
     <div className="pagination">
       <ul>
